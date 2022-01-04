6ad86aae5f390d33e40852d40997877368232797
Justin Kotalik <jukotali@microsoft.com>
2022-01-04 15:32:58 -0800

    nit

--- src/DeploymentEngine/Controllers/WeatherForecastController.cs
+++ src/DeploymentEngine/Controllers/WeatherForecastController.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Azure.Deployments.Core.Definitions;
+using Azure.Deployments.Core.Helpers;
+using Azure.Deployments.Core.Json;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeploymentEngine.Controllers;
@@ -19,14 +23,50 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var deploymentPayload = System.IO.File.ReadAllText("");
+        var httpContent = new StringContent(deploymentPayload, Encoding.UTF8, "application/json");
+
+    }
+
+    /// <summary>
+    /// Deserialize a deployment http request payload into a DeploymentContent object and try to calculate the hash.
+    /// </summary>
+    /// <param name="httpContent">The HTTP Content.</param>
+    /// <param name="httpConfiguration">The HTTP Configuration.</param>
+    /// <returns>The requested deployment definition.</returns>
+    private static async Task<DeploymentContent> GetDeploymentContentAndTryCalculateHash(HttpContent httpContent)
+    {
+        var deploymentContent = await ReadAsJsonAsyncWithRewind<DeploymentContent>(httpContent)
+            .ConfigureAwait(continueOnCapturedContext: false);
+
+        deploymentContent.Properties.TemplateHash = deploymentContent.Properties.Template != null
+            ? TemplateHelpers.ComputeTemplateHash(deploymentContent.Properties.Template.ToJToken())
+            : null;
+
+        return deploymentContent;
+    }
+
+    private static async Task<T> ReadAsJsonAsyncWithRewind<T>(HttpContent httpContent)
+    {
+        var contentStream = await httpContent.ReadAsStreamAsync().ConfigureAwait(continueOnCapturedContext: false);
+        var streamPosition = contentStream.Position;
+
+        try
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            var formatters = new MediaTypeFormatter[] {
+                new JsonMediaTypeFormatter { SerializerSettings = SerializerSettings.SerializerMediaTypeSettings, UseDataContractJsonSerializer = false } };
+
+            return await httpContent.ReadAsAsync<T>(formatters)
+                .ConfigureAwait(continueOnCapturedContext: false);
+        }
+        finally
+        {
+            if (streamPosition != contentStream.Position)
+            {
+                contentStream.Seek(streamPosition, SeekOrigin.Begin);
+            }
+        }
     }
 }
