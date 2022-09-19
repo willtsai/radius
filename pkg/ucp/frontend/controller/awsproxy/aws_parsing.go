b1f695f83ee30011d13adc7e99bb8931beef6755
vinayada1 <28875764+vinayada1@users.noreply.github.com>
2022-09-19 11:37:52 -0700

    more changes

--- pkg/ucp/frontend/controller/awsproxy/aws_parsing.go
+++ pkg/ucp/frontend/controller/awsproxy/aws_parsing.go
@@ -8,31 +8,84 @@ package awsproxy
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudcontrol"
 	"github.com/project-radius/radius/pkg/middleware"
+	awserror "github.com/project-radius/radius/pkg/ucp/aws"
+	ctrl "github.com/project-radius/radius/pkg/ucp/frontend/controller"
 	"github.com/project-radius/radius/pkg/ucp/resources"
+	"github.com/project-radius/radius/pkg/ucp/rest"
 )
 
-func ParseAWSRequest(ctx context.Context, basePath string, r *http.Request) (*cloudcontrol.Client, string, resources.ID, error) {
-	// Common parsing in AWS plane requests
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil {
-		return nil, "", resources.ID{}, err
+const (
+	AWSClientKey       = "AWSClient"
+	AWSResourceTypeKey = "AWSResourceType"
+	AWSResourceID      = "AWSResourceID"
+)
+
+// AWSParser is the controller implementation to create/update an AWS resource.
+type AWSParser struct {
+	ctrl.BaseController
+}
+
+// NewAWSParser creates a new AWSParser.
+func NewAWSParser(opts ctrl.Options) (*AWSParser, error) {
+	return &AWSParser{ctrl.NewBaseController(opts)}, nil
+}
+
+func (p *AWSParser) Parse(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// Common parsing in AWS plane requests
+		ctx := r.Context()
+		cfg, err := config.LoadDefaultConfig(ctx)
+		if err != nil {
+			handleError(ctx, w, r, err)
+			return
+		}
+
+		cfg.ClientLogMode |= aws.LogRequestWithBody
+		cfg.ClientLogMode |= aws.LogResponseWithBody
+
+		client := cloudcontrol.NewFromConfig(cfg)
+		path := middleware.GetRelativePath(p.Options.BasePath, r.URL.Path)
+		id, err := resources.ParseByMethod(path, r.Method)
+		if err != nil {
+			handleError(ctx, w, r, err)
+			return
+		}
+
+		parts := []string{}
+		for _, segment := range id.TypeSegments() {
+			parts = append(parts, strings.ReplaceAll(strings.ReplaceAll(segment.Type, ".", "::"), "/", "::"))
+		}
+
+		resourceType := strings.Join(parts, "::")
+		ctx = context.WithValue(ctx, AWSClientKey, client)
+		ctx = context.WithValue(ctx, AWSResourceTypeKey, resourceType)
+		ctx = context.WithValue(ctx, AWSResourceID, id)
+		r = r.WithContext(ctx)
+		h.ServeHTTP(w, r)
 	}
 
-	cfg.ClientLogMode |= aws.LogRequestWithBody
-	cfg.ClientLogMode |= aws.LogResponseWithBody
+	return http.HandlerFunc(fn)
+}
 
-	client := cloudcontrol.NewFromConfig(cfg)
-	path := middleware.GetRelativePath(basePath, r.URL.Path)
-	id, err := resources.ParseByMethod(path, r.Method)
-	if err != nil {
-		return nil, "", resources.ID{}, err
+func handleError(ctx context.Context, w http.ResponseWriter, r *http.Request, err error) {
+	errResponse, handleErr := awserror.HandleAWSError(err)
+	if handleErr != nil {
+		e := rest.ErrorResponse{
+			Error: rest.ErrorDetails{
+				Code:    "Invalid",
+				Message: err.Error(),
+			},
+		}
+		errResponse = rest.NewInternalServerErrorARMResponse(e)
 	}
-
-	resourceType := resources.ToARN(id)
-	return client, resourceType, id, nil
+	applyErr := errResponse.Apply(r.Context(), w, r)
+	if applyErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
