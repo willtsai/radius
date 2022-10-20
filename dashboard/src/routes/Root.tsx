161b0e89318090342d6e33cc4228d18e34ea83ff
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2022-10-20 15:50:45 -0700

    Add navbar

--- dashboard/src/routes/Root.tsx
+++ dashboard/src/routes/Root.tsx
@@ -1,29 +1,38 @@
 import { Link, NavLink, Outlet, useNavigation } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from "react-bootstrap/Navbar";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faCube, faPager, faCubes, faBorderAll } from '@fortawesome/free-solid-svg-icons';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Root.css';
 
 export default function Root() {
   const navigation = useNavigation();
   return (
     <>
+      <Navbar bg="primary" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/">Project Radius</Navbar.Brand>
+        </Container>
+      </Navbar>
       <div className="Root-container">
         <div className="Root-sidebar">
           <h1>Radius Dashboard</h1>
           <nav>
             <ul>
               <li>
-                <Link to="/" >Home</Link>
+                <NavLink to={`environments`}><FontAwesomeIcon icon={faCube} />  Environments</NavLink>
               </li>
               <li>
-                <NavLink to={`environments`}>Environments</NavLink>
+                <NavLink to={`applications`}><FontAwesomeIcon icon={faPager} />  Applications</NavLink>
               </li>
               <li>
-                <NavLink to={`applications`}>Applications</NavLink>
+                <NavLink to={`containers`}><FontAwesomeIcon icon={faCubes} />  Containers</NavLink>
               </li>
               <li>
-                <NavLink to={`containers`}>Containers</NavLink>
-              </li>
-              <li>
-                <NavLink to={`resources`}>Resources</NavLink>
+                <NavLink to={`resources`}><FontAwesomeIcon icon={faBorderAll} />  Resources</NavLink>
               </li>
             </ul>
           </nav>
