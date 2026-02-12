890d59836e888c1a02f88afc0c7f3485947e1f92
Will Tsai <28876888+willtsai@users.noreply.github.com>
2026-02-11 17:32:05 -0800

    add rendering of resource type in app graph for gh action
    Signed-off-by: Will Tsai <28876888+willtsai@users.noreply.github.com>

--- actions/app-graph-diff/scripts/render.sh
+++ actions/app-graph-diff/scripts/render.sh
@@ -166,10 +166,10 @@ render_mermaid() {
 graph LR
 EOF
 
-    # Render resource nodes (name + type on separate lines)
-    echo "${diff}" | jq -r '.addedResources[] | "    \(.id | gsub("[^a-zA-Z0-9]"; "_"))[\"\(.name // .id)<br/><i>\(.type // \"\")</i>\"]:::added"'
-    echo "${diff}" | jq -r '.removedResources[] | "    \(.id | gsub("[^a-zA-Z0-9]"; "_"))[\"\(.name // .id)<br/><i>\(.type // \"\")</i>\"]:::removed"'
-    echo "${diff}" | jq -r '.modifiedResources[] | "    \(.id | gsub("[^a-zA-Z0-9]"; "_"))[\"\(.name // .id)<br/><i>\(.type // \"\")</i>\"]:::modified"'
+    # Render resource nodes (name + type in parentheses)
+    echo "${diff}" | jq -r '.addedResources[] | "    " + (.id | gsub("[^a-zA-Z0-9]"; "_")) + "[\"" + (.name // .id) + " (" + (.type // "") + ")\"]:::added"'
+    echo "${diff}" | jq -r '.removedResources[] | "    " + (.id | gsub("[^a-zA-Z0-9]"; "_")) + "[\"" + (.name // .id) + " (" + (.type // "") + ")\"]:::removed"'
+    echo "${diff}" | jq -r '.modifiedResources[] | "    " + (.id | gsub("[^a-zA-Z0-9]"; "_")) + "[\"" + (.name // .id) + " (" + (.type // "") + ")\"]:::modified"'
 
     # Render connection edges with link styles
     # Track link index for linkStyle directives (Mermaid numbers links sequentially)
